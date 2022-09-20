0d93e13f94a43f310e9763cd82512be702febaed
mkeloo <mkeloo@ufl.edu>
2022-09-20 00:46:05 -0400

    first commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <div>
+        <h2>Skills</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Work = () => {
+  return (
+    <div>
+      <div>
+        <h2>Work</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
